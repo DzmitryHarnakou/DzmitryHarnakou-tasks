1e6c0da201e19797011200e2408b015c168d5d4a
DzmitryHarnakou <dzmitry.harnakou@gmail.com>
2018-11-02 16:09:48 +0300

    Последние цифры числа

--- Task_1_js/1_4/scrypt.js
+++ Task_1_js/1_4/scrypt.js
@@ -1,16 +1,23 @@
 //Серия чисел, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317 (^ - степень числа). Найдите 10 последних цифр результата такой последовательности,
 //для заданного n. 1 <= n <= 1000
 
+//функция умножения, которая выводит только 10 последних цифр операции
 
-function powm(m, l){
-  summ = 0;
-  var n = 1;
-  var finResult;
+
+function sum(l){
+var sum = 0;
+for (var n =1; n <= l; n++) {
   var result = 1;
-  for (n ; n <= m ; n++){
-      result *= l;
+  var topLimit = 100000000000;
+  for (var m=1; m <= n; m++) {
+    result *= n;
+    if (result >= topLimit) {
+      result = result%10000000000;
+    }
   }
-  return result;
+  sum +=result;
+}
+return sum;
 }
 
-console.log (powm(4,4));
+alert (sum(10));
