80781006a6e7302aa89c794c77aca611fc5195fb
DzmitryHarnakou <dzmitry.harnakou@gmail.com>
2018-11-06 15:39:30 +0300

    Not final without UI.

---
+++ task_2_js/class/class.js
@@ -0,0 +1,50 @@
+class MusicalInstrument {
+	constructor(type, name, model, availible) {
+	this.type = type;
+	this.name = name;
+	this.model = model;
+	this.availible = availible;
+	}
+	
+	play () {
+		console.log (this.name+" "+this.model+" "+"is playing");
+	}
+	showType () {
+		console.log (this.name+" "+this.model+" "+"is"+this.type);
+	}	
+
+}
+
+class Guitar extends MusicalInstrument {
+	constructor(type, name, model, availible, strings) {
+		super(type, name, model, availible);
+	this.strings = strings;
+	}
+	showStrings() {
+		console.log (`${this.model} has ${this.strings} strings`);
+	}
+
+}
+
+class Drums extends MusicalInstrument {
+	constructor(type, name, model, availible, set) {
+		super (type, name, model, availible);
+		this.set = set;
+	}
+
+	showSet () {
+		console.log (this.model+' Consist of '+this.set);
+	}
+
+	play () {
+		super.play(); //invoke parent method 
+		console.log (`${this.name} ${this.model} plays very loud`);
+	}	
+}
+
+var guitar1 = new Guitar ("Ibanez","RG 7620", 7, true, "Gutar");
+guitar1.showStrings();
+
+var drums1 = new Drums ("Drums", "SONOR", "507", false, "5 drums + 3 cymbals");
+drums1.showSet();
+drums1.play();
---
+++ task_2_js/proto/proto.js
@@ -0,0 +1,48 @@
+function MusicalInstrument (type, name, model, availible ){
+	this.type = type;
+	this.name = name;
+	this.model = model;
+	this.availible = availible;
+}
+
+MusicalInstrument.prototype.play = function () {
+	console.log (name+" "+model+" "+"is playing");
+}
+MusicalInstrument.prototype.showType = function () {
+	console.log (name+" "+model+" "+"is"+this.type);
+}
+
+function Guitar(name, model, strings,availible,type) {
+	this.strings = strings;
+	MusicalInstrument.prototype.constructor(type, name, model, availible);
+}
+
+Guitar.prototype = Object.create(MusicalInstrument.prototype);
+
+Guitar.prototype.showStrings = function() {
+	console.log (`${this.model} has ${this.strings} strings`);
+}
+
+function Drums(type, name, model, availible, set ) {
+	MusicalInstrument.prototype.constructor(type, name, model, availible);
+	this.set = set;
+}
+
+Drums.prototype = Object.create(MusicalInstrument.prototype);
+
+Drums.prototype.showSet = function() {
+	console.log (this.model+' Consist of '+this.set);
+}
+
+// Override
+Drums.prototype.play = function () {
+	
+	console.log (`${this.name} ${this.model} plays very loud`);
+}
+
+var guitar1 = new Guitar ("Ibanez","RG 7620", 7, true, "Gutar");
+guitar1.showStrings();
+
+var drums1 = new Drums ("Drums", "SONOR", "507", false, "5 drums + 3 cymbals");
+drums1.showSet();
+drums1.play();
